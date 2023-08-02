2f9bd6c4090d1b6455ece85c3052da22931c7375
Om Sharma <omsharma.theplayer11@gmail.com>
2023-08-02 15:59:07 +0530

    navbar structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
           <StarsCanvas />
         </div>
        </div> 
-
     </BrowserRouter>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+// import { logo, menu, close } from "../assets";
+import { theplayer9 } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2 bg-black"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={theplayer9}
+            alt="logo"
+            className="w-15 h-10 object-contain "
+          />
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10 ">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }  hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              {console.log(link)}
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
